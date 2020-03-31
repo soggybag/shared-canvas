321c485ae371fd22bd0754be4fbac10087b7fd52
Mitchell <soggybag@gmail.com>
2020-03-31 11:12:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,57 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+
+// Mitchell 
 ctx.beginPath()
 ctx.rect(230, 30, 40, 90)
 ctx.fillStyle = '#ff00ff'
 ctx.fill()
+
+
+// Franklin
+
+
+
+// Beck
+
+
+
+// John
+
+
+
+// Redi
+
+
+
+// Stasi
+
+
+
+// Aucoeur
+
+
+
+// Audi
+
+
+
+
+// Muhammad
+
+
+
+// Anthony
+
+
+
+// Betsy
+
+
+
+// Shaan
+
+
+
+// Farhan
