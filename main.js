6a79234dfc04a9aa801434bd61307b0774be60b9
Mitchell <soggybag@gmail.com>
2020-10-23 10:29:46 -0700

    updated

--- main.js
+++ main.js
@@ -2,16 +2,9 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const size = 113
-const test = 'Hello World'
-
-function getLocation(x, y) {
-	return { x: x * size, y: y * size }
-}
-
-console.log('???')
 
 // Default export ONLY ONE!
 export default ctx
 // named export 
-export { size, test, getLocation }
+export { size }
 
