ae5fc998b4c64c0555feadf2abeecf2ed5560acd
Kash <amahlw@gmail.com>
2020-10-26 18:48:52 -0400

    Update kash.js

--- kash.js
+++ kash.js
@@ -1,11 +1,24 @@
-const kash = canvas.getContext('2d')
+import kash, { size } from './main.js'
+
+const x = 1 * size
+const y = 3 * size
+
 
 
 kash.beginPath()
-kash.lineWidth = "16";
+kash.lineWidth = "5";
 kash.strokeStyle = "green";
-kash.rect(111, 3, 120, 93, 13);
+kash.rect(x, y, size, size);
 kash.stroke();
-kash.shadowBlur = 20;
+kash.shadowBlur = 22;
+kash.shadowColor = "blue";
+kash.fill()
+
+kash.beginPath()
+kash.lineWidth = "26";
+kash.strokeStyle = "yellow";
+kash.arc(x + size / 2, y + size / 2 + 3, 30, 0, Math.PI * 2)
+kash.stroke();
+kash.shadowBlur = 22;
 kash.shadowColor = "blue";
 kash.fill()
