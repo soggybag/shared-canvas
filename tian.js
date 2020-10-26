38b8799cadda111a862c772b2e4607f3070835da
Tian Qiu <tian.qiu142@gmail.com>
2020-10-26 15:47:07 -0700

    Update tian.js

--- tian.js
+++ tian.js
@@ -5,8 +5,7 @@ ctx.restore();
 const x = 4 * size;
 const y = 4 * size;
 
-ctx.fillStyle = 'purple';
-ctx.fill();
+// ctx.fillStyle = 'purple';
 
 ctx.beginPath();
 ctx.arc(175, 175, 50, 0, Math.PI * 2, true); // Outer circle
@@ -16,3 +15,5 @@ ctx.arc(175, 175, 25, 0, Math.PI, false); // Mouth (clockwise)
 ctx.arc(160, 155, 5, 0, Math.PI * 2, true); // Left eye
 
 ctx.arc(190, 165, 15, 0, Math.PI * 2, true); // Right eye
+
+ctx.fill();
