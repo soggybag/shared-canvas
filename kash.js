55c90ce411f6e5a84ff00272a6215fdcc77eb5c1
Kash <amahlw@gmail.com>
2020-10-28 18:06:50 -0400

    Update kash.js

--- kash.js
+++ kash.js
@@ -1,24 +1,22 @@
 import kash, { size } from './main.js'
 
-const x = 1 * size
-const y = 3 * size
+const x = 1 * size;
+const y = 3 * size;
 
-
-
-kash.beginPath()
-kash.lineWidth = "5";
-kash.strokeStyle = "green";
+kash.beginPath();
+kash.lineWidth = '5';
+kash.strokeStyle = 'green';
 kash.rect(x, y, size, size);
 kash.stroke();
 kash.shadowBlur = 22;
-kash.shadowColor = "blue";
-kash.fill()
+kash.shadowColor = 'blue';
+kash.fill();
 
-kash.beginPath()
-kash.lineWidth = "26";
-kash.strokeStyle = "yellow";
-kash.arc(x + size / 2, y + size / 2 + 3, 30, 0, Math.PI * 2)
+kash.beginPath();
+kash.lineWidth = '26';
+kash.strokeStyle = 'yellow';
+kash.arc(x + size / 2, y + size / 2 + 3, 30, 0, Math.PI * 2);
 kash.stroke();
 kash.shadowBlur = 22;
-kash.shadowColor = "blue";
-kash.fill()
+kash.shadowColor = 'blue';
+kash.fill();
