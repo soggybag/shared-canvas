a8ee1c7b468d2e2acb8f055380953afc5195efa1
tian142 <69370853+tian142@users.noreply.github.com>
2020-11-09 17:11:06 -0800

    Final Update, everything fixed for tian

--- tian.js
+++ tian.js
@@ -1,18 +1,87 @@
-import ctx, { size } from './mian.js';
-
-ctx.restore();
+import ctx, { size } from './main.js';
 
 const x = 4 * size;
 const y = 4 * size;
 
+ctx.beginPath();
+ctx.rect(x, y, size, size);
+ctx.fillStyle = '#6d29ff';
 ctx.fill();
 
 ctx.beginPath();
-ctx.arc(x + size / 2, y + size / 2, 50, 0, Math.PI * 2, true); // Outer circle
+ctx.arc(x + size / 2, y + size / 2, 40, 0, Math.PI * 2);
+ctx.fillStyle = '#ffdb3b';
+ctx.fill();
 
-ctx.arc(x + size / 2, y + 80, 25, 0, Math.PI, false); // Mouth (clockwise)
+ctx.beginPath();
+ctx.arc(x + size / 2, y + size / 2, 38, 0, Math.PI * 2);
+ctx.fillStyle = 'black';
+ctx.fill();
 
-ctx.arc(x + 70, y + 80, 10, 0, Math.PI * 2, true); // Left eye
+ctx.beginPath();
+ctx.arc(x + size / 2, y + size / 2, 36, 0, Math.PI * 2);
+ctx.fillStyle = '#ffdb3b';
+ctx.fill();
 
-ctx.arc(x + size / 2, y + 37, 30, 0, Math.PI * 2, true); // Right eye
-ctx.stroke();
+ctx.beginPath();
+ctx.rect(x + 43, y + 40, 9, 34);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 43, y + 40, -5, 4);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 43, y + 70, -5, 4);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 51, y + 40, 15, 4);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 51, y + 70, 15, 4);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 48, y + 73, 4, 9);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.beginPath();
+ctx.rect(x + 48, y + 33, 4, 10);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 58, y + 73, 4, 9);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.beginPath();
+ctx.rect(x + 58, y + 33, 4, 10);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 63, y + 40, 5, 34);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 65, y + 47, 7, 7);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 48, y + 56, 20, 4);
+ctx.fillStyle = 'black';
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(x + 65, y + 62, 7, 7);
+ctx.fillStyle = 'black';
+ctx.fill();
